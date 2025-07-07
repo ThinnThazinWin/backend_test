7568faf0d1661f7321c74d5968cb72a51eedd713
thinnthazinwin <thinnthazinwin@agbcommunication.com>
2025-07-07 13:33:27 +0630

    Updating vercel.json

---
+++ constants/headers/index.js
@@ -0,0 +1,15 @@
+exports.users_header = [
+  {
+    header: "_id",
+    accessorKey: "_id",
+  },
+  {
+    header: "Name",
+    accessorKey: "name",
+  },
+  {
+    header: "Email",
+    accessorKey: "email",
+  },
+
+];
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const users_header =  require('../constants/headers')
 
 // GET all users
 router.get('/', async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+
+    const columns = users_header;
+    const page = parseInt(req?.query?.page) || 1;
+    const limit = parseInt(req?.query?.limit) || 10;
+    const startWith = (page - 1) * limit;
+    const totalCount = await User.countDocuments();
+    const users = await User.find({}, { updatedAt: 0, createdAt: 0, __v: 0 })
+        .skip(startWith)
+        .limit(limit)
+        .sort({ posteddate: 1 })
+        .lean();
+    res.status(200).send({
+        columns,
+        data: users,
+        count: totalCount
+    })
 });
 
 // POST create new user
 router.post('/', async (req, res) => {
-  const newUser = new User(req.body);
-  const savedUser = await newUser.save();
-  res.json(savedUser);
+    const newUser = new User(req.body);
+    const savedUser = await newUser.save();
+    res.json(savedUser);
 });
 
 module.exports = router;
