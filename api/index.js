5211e2c409be97f7fd86162a276acf870e973e91
thinnthazinwin <thinnthazinwin@agbcommunication.com>
2025-07-07 12:05:28 +0630

    Updating vercel.json

--- api/index.js
+++ api/index.js
@@ -1,17 +1,26 @@
 // api/index.js
 
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('../config/db');
-const userRoutes = require('../routes/userRoutes');
-const serverless = require('serverless-http');
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const connectDB = require('../config/db');
+// const userRoutes = require('../routes/userRoutes');
+// const serverless = require('serverless-http');
 
-dotenv.config();
-connectDB();
+// dotenv.config();
+// connectDB();
 
+// const app = express();
+// app.use(express.json());
+// app.use('/api/users', userRoutes);
+
+// // Don't call app.listen
+// module.exports.handler = serverless(app);
+
+const express = require("express");
 const app = express();
-app.use(express.json());
-app.use('/api/users', userRoutes);
 
-// Don't call app.listen
-module.exports.handler = serverless(app);
+app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
